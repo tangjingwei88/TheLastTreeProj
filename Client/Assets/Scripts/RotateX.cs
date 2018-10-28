bfb4289329a896c25117916bfea069ddfe672dd3
tangjingwei88 <jingweitang88@gmail.com>
2018-10-29 00:44:27 +0800

    添加黑白素材

---
+++ Client/Assets/Scripts/RotateX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateX : MonoBehaviour {
+
+    public int speed = 500;
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(new Vector3(1,0,0),2);
+	}
+}
